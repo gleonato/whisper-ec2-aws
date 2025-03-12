3278ee0338881475ac2dd70afb1f81f47eb8ee87
Gus Leonato <gleonato@gmail.com>
2025-03-12 09:27:07 -0300

    sh script to run in batch mode

--- py/run_script.sh
+++ py/run_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Shell to run on ec2 instance
-# This script will run the python script whisper_bulk_aws2.py and log the output to output.log
-python /home/ec2-user/whisper-ec2-aws/py/whisper_bulk_aws2.py > output.log 2>&1
+# This script will run the python script whisper_bulk_aws.py and log the output to output.log
+nohup python /home/ec2-user/whisper-ec2-aws/py/whisper_bulk_aws.py > output.log 2>&1 &
+
+# Print the process ID so you can monitor or kill it later
+echo "Process started with PID $!"
